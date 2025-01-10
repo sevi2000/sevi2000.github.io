7060e7dad0c4d6e4e457865b73a69d8b8e55d7f4
DERVISHI <sevidervishi@outlook.fr>
2025-01-10 14:42:20 +0100

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
         </div>
         <p className="text-gray-500 mt-4">Feel free to reach me out </p>
         <div className="flex items-centerflex space-x-4">
-            <a href="mailto:sevidervishi@outlook.fr"><FaEnvelope/> sevidervishi@outlook.fr</a>
-            <a><FaPhone></FaPhone>Tel +33 6 43 08 94 32</a>
-            <a href="https://www.linkedin.com/in/sevi-dervishi-70a85723a/" target="_blank"><FaLinkedinIn></FaLinkedinIn>LinkedIn</a>
+            <a href="mailto:sevidervishi@outlook.fr" className="display flex"><FaEnvelope/> sevidervishi@outlook.fr</a>
+            <a className="display flex"><FaPhone></FaPhone>Tel +33 6 43 08 94 32</a>
+            <a href="https://www.linkedin.com/in/sevi-dervishi-70a85723a/" target="_blank" className="display flex"><FaLinkedinIn></FaLinkedinIn>LinkedIn</a>
         </div>
 </>
     );
