b1350fe6e2d2214594bbd7999fa1169a3fa1d820
SEVI DERVISHI <sevidervishi@outlook.fr>
2025-11-05 13:13:19 +0100

    Fixed title bug

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,8 @@
 
-export default function PrrojectCard({ title, description, link }) {
+export default function PrrojectCard({ name, description, link }) {
   return (
     <div className="bg-gray-900 p-6 rounded-2xl shadow-lg hover:shadow-teal-400/20 transition">
-      <h4 className="text-xl font-bold mb-2">{title}</h4>
+      <h4 className="text-xl font-bold mb-2">{name}</h4>
       <p className="text-gray-400 mb-4">{description}</p>
       <a
         href={link}
